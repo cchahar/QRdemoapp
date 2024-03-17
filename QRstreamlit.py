01e87e318ebf27eaa28360379df747a628e8eeb4
cchahar <cchahar@gmail.com>
2024-03-17 22:23:23 +0530

    Update QRstreamlit.py

--- QRstreamlit.py
+++ QRstreamlit.py
@@ -1,43 +1,7 @@
-import cv2
 import streamlit as st
-from pyzbar.pyzbar import decode
+from streamlit_qrcode_scanner import qrcode_scanner
 
-def decode_qr_code(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    decoded_objects = decode(gray)
-    return decoded_objects
+qr_code = qrcode_scanner(key='qrcode_scanner')
 
-
-st.title("QR Code Scanner")
-
-cap = cv2.VideoCapture(0)
-
-cap.set(3, 640)
-cap.set(4, 480)
-
-stframe = st.empty()
-stop_button = st.button("Stop")
-
-while cap.isOpened():
-    success, frame = cap.read()
-    if not success:
-        st.error("Failed to capture frame.")
-        break
-
-    # Display the frame
-    stframe.image(frame, channels="BGR")
-
-    # Decode QR code
-    #decoded_objects = decode_qr_code(frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    decoded_objects = decode(gray)
-    if decoded_objects:
-        for obj in decoded_objects:
-            st.success(f"QR Code Detected: {obj.data.decode('utf-8')}")
-
-    if stop_button:
-        break
-
-cap.release()
-
-
+if qr_code:
+st.write(qr_code)
